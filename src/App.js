c5379325080fd28c63ae956e3d3fdc7a47ad2927
Randall Lu <randalllu02@gmail.com>
2018-11-03 12:50:51 -0700

    some sample code of frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
 
 class App extends Component {
   constructor(props) {
@@ -32,22 +42,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button onClick={this.fancy}>click me!!</button>
-        </header>
-        <p>{this.state.response}</p>
+        <Paper className = "paper container">
+          <Avatar className = "website Avatar">
+          </Avatar>
+          <Typography Component='h2' variant='h4'>
+            Log in
+          </Typography>
+        </Paper>
       </div>
     );
   }
